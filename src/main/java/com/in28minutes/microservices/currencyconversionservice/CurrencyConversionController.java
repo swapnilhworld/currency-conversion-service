92fed7ead6fc7b8166659568481c0d4ec68e242f
swapnilhworld <swapnilhworld@gmail.com>
2024-03-25 23:24:32 +0530

    added feign client

--- src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyConversionController.java
+++ src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -3,6 +3,7 @@ package com.in28minutes.microservices.currencyconversionservice;
 import java.math.BigDecimal;
 import java.util.HashMap;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,6 +12,26 @@ import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class CurrencyConversionController {
+	
+	@Autowired
+	private CurrencyExchangeProxy proxy;
+	
+	@GetMapping("/currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
+	public CurrencyConversion calculateCurrencyConversionFeign(
+			@PathVariable String from,
+			@PathVariable String to,
+			@PathVariable BigDecimal quantity
+			) {
+		
+		
+		CurrencyConversion currencyConversion = proxy.retrieveExchangeValue(from, to);
+		
+		return new CurrencyConversion(
+				currencyConversion.getId(),
+				from, to, quantity, BigDecimal.ONE, BigDecimal.ONE, 
+				currencyConversion.getEnvironment()+ "-feign");
+	}
+	
 	
 	@GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
 	public CurrencyConversion calculateCurrencyConversion(
@@ -32,7 +53,7 @@ public class CurrencyConversionController {
 		return new CurrencyConversion(
 				currencyConversion.getId(),
 				from, to, quantity, BigDecimal.ONE, BigDecimal.ONE, 
-				currencyConversion.getEnvironment());
+				currencyConversion.getEnvironment()+"-Rest template");
 	}
 
 }
