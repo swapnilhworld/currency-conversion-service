3790cb0778deebf4cdf37802d24974e1fb7e3c79
swapnilhworld <swapnilhworld@gmail.com>
2024-03-30 16:35:26 +0530

    modified - 23

--- src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyExchangeProxy.java
+++ src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyExchangeProxy.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 
-@FeignClient(name="currency-exchange", url="localhost:8000")
+@FeignClient(name="currency-exchange-service")
 public interface CurrencyExchangeProxy {
 
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
 	public CurrencyConversion retrieveExchangeValue(
 			@PathVariable String from,
-			@PathVariable String to) ;
+			@PathVariable String to);
 }
